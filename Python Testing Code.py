0bd8737a27b818c8a2ea952e95c2f53f8dc93c45
Izyan Khan <izyankhan@MacBookPro.lan>
2026-07-23 20:41:59 -0700

    Implement Tests

--- Python Testing Code.py
+++ Python Testing Code.py
@@ -1,10 +1,6 @@
-print("Login")
-name = input("What is your name?")
-while name != "Izyan":
-   print("Sorry your name does not seem to be in our system please register at www.notarealwebsite.com or Try again.")
-   name = input("What is your name?")
-print("Please enter your password below")
-password = input("Password :")
-while password != "Max":
-   print("Invalid password please try again")
-   password = input("Password :")
+names = ["Aiza", "Izi", "Chachu"]
+name = input("What is your name? :")
+if name in names:
+   print("Access granted")
+else:
+   print("Access not granted")
