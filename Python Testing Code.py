d47349eaa8e0957a50d98f26a71855ae52131d71
Izyan Khan <izyankhan@MacBookPro.lan>
2026-07-30 18:24:02 -0700

    Implement Tests

--- Python Testing Code.py
+++ Python Testing Code.py
@@ -1,4 +1,24 @@
+print("Hello Welcome Please Login below")
+name = input("Name :")
+verified_name = "Izyan"
+while name != verified_name:
+    print("Sorry I dont see your name Please try again")
+    name = input("Name :")
+print("Please Enter your password below")
+password = input("Password :")
+verified_password = "Welcome7643"
+while password != verified_password:
+    print("Incorrect Password")
+    password = input("Password :")
+print("Welcome![before we continue you have to guess the correct questions]")
 import random
-colors = ["red", "Green", "Blue"]
+colors = ["Red","Blue","Green"]
+for i in range(len(colors)):
+    print(i + 1, colors[i])
 result = random.choice(colors)
+answer = input("Enter your answer here :")
+while answer != random.choice:
+    print("Sorry That is incorrect please try again")
+    answer = input("Enter your answer here :")
+print("That is correct!")
 print(result)
