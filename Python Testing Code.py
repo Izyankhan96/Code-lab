e5eb7d9269c9f9cb796426ca16741929a9d87930
Izyan Khan <izyankhan@MacBookPro.lan>
2026-08-23 08:45:15 -0700

    Implement Code

--- Python Testing Code.py
+++ Python Testing Code.py
@@ -1,34 +1,37 @@
-print("Hello user this is a place where you can add tasks, delete tasks and view them!")
-print("Login")
-name = input("Username :")
-while name != "Izyan":
-    print("Sorry Invalid username")
-    name = input("Username :")
-print("Welcome back Izyan!")
-password = input("Password :")
-while password != "123456789":
-    print("Sorry Invalid Password")
-    password = input("Password :")
-print("Login Successful!")
-print("Here are your tasks for today :")
-tasks = ["Finish Book report", "Code a project", "Clean the house"]
+print("Welcome to my Task Manager!")
+login = input("Please enter your username :")
+username = "Izi"
+while login != username:
+    print("Invalid Username, Please try again.")
+    login = input("Please enter your username :")
+print("Welcome back, Izi!")
+password = input("Please enter your password :")
+user_password = "1234"
+while password != user_password:
+    print("Invalid Password, Please try again.")
+    password = input("Please enter your password :")
+print("You have successfully logged in!")
+tasks = ["Clean the house", "Do the laundry", "Finish the report","Pratice Python"]
+print("Your tasks for today :")
 for i in range(len(tasks)):
     print(i + 1, tasks[i])
-option1 = input("Would you like to add something or delete something from your list? say yes to add, delete to delete a task and no to keep things as they are :")
-if option1.lower() == "yes":
-   new_task = input("Enter your new task :")
-   tasks.append(new_task)
-   print("Here are your updated tasks for today :")
-for i in range(len(tasks)):
+print("if you would like to add or change a task please select from the following options below :")
+options = ["Add","Delete","None"]
+for i in range(len(options)):
+    print(i + 1, options[i])
+options_choice = input("Please select an option : ")
+if options_choice == "Add":
+    new_task = input("Please enter the new task you would like to add :")
+    tasks.append(new_task)
+    print("Your updated tasks for today are :")
+    for i in range(len(tasks)):
         print(i + 1, tasks[i])
-if option1.lower() == "no":
-    print("Alright no changes " \
-    "made.")
-if option1 == "delete":
-     delete_task = input("Enter the task you want to delete :")
-     tasks.remove(delete_task)
-     print("Here are your updated tasks for today :")
-for i in range(len(tasks)):
-        print(i + 1, tasks[i])
-        
+if options_choice == "Delete":
+    delete_task = input("Please enter the task number you would like to delete :")
+    tasks.remove(delete_task)
+    print("Your updated tasks for today are :")
+    for i in range(len(tasks)):
+            print(i + 1, tasks[i])
     
+if options_choice == "None":
+    print("No changes made to your tasks.")
